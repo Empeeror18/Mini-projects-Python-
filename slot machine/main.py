e5ed4e3eb8a8fcb85d52e64d3315a3cbb227da0d
empeeror <saryal878@gmail.com>
2025-11-17 20:51:11 +0545

    Initial commit

---
+++ slot machine/main.py
@@ -0,0 +1,13 @@
+def deposit():
+    while True:
+        try:
+            amount = input("Enter the amount to be deposited: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount < 0:
+                print ("Amount must be greater than 0.")
+            else:
+                break
+        else:
+            print("Enter a valid number!")
+    return amount
